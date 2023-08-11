e9031a870a796d262862eded3b89cf209c7e856d
zeekcheung <57971469+zeekcheung@users.noreply.github.com>
2023-08-11 22:49:32 +0800

    Update 4.install_golang.fish

--- scripts/ubuntu/fishshell/4.install_golang.fish
+++ scripts/ubuntu/fishshell/4.install_golang.fish
@@ -7,7 +7,7 @@ curl -0 https://go.dev/dl/go1.21.0.linux-amd64.tar.gz
 sudo tar -C /usr/local -xzf go1.21.0.linux-amd64.tar.gz
 
 # Add Golang's binary directory to fish shell's path
-echo 'set PATH $PATH /usr/local/go/bin' >> ~/.config/fish/config.fish
+echo 'set PATH $PATH $HOME/go/bin' >> ~/.config/fish/config.fish
 source ~/.config/fish/config.fish
 
 # Verify the installation
