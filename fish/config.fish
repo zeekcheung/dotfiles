4668afb473066874e15f2da5f94d66ceb619f632
zeekcheung <57971469+zeekcheung@users.noreply.github.com>
2023-08-19 11:55:59 +0800

    add proxy config on wsl2 with fish shell

--- fish/config.fish
+++ fish/config.fish
@@ -10,6 +10,22 @@ function fish_postcmd --on-event fish_postexec
 end
 # Fish end
 
+# Proxy
+function proxy 
+	set -l host_ip (cat /etc/resolv.conf |grep "nameserver" |cut -f 2 -d " ")
+	set -gx http_proxy $host_ip:10809
+	set -gx https_proxy $host_ip:10809
+	echo "Proxy enabled"
+end
+
+function unproxy 
+	set -e http_proxy
+	set -e https_proxy
+	echo "Proxy disabled"
+end
+# Proxy end
+
+
 # Starship
 set -gx STARSHIP_CONFIG ~/.config/starship/config.toml
 starship init fish | source
