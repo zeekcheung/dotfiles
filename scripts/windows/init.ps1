71780d6371afab440e0a55313d14ec34dc30e640
zeekcheung <zeekcheung@gmail.com>
2024-01-14 12:51:32 +0800

    refactor(scripts): revamp init.ps1

--- scripts/windows/init.ps1
+++ scripts/windows/init.ps1
@@ -1,43 +1,67 @@
+$APPDATA = $Env:APPDATA
+$LOCALAPPDATA = $ENV:LOCALAPPDATA
+$CONFIG = "$Env:USERPROFILE\.config"
+$DOCUMENTS = "$Env:USERPROFILE\Documents"
+
 # Directory symbolic link list: sourceDir => targetDir
 $dirLinks = @{
-  "$Env:APPDATA\alacritty" = "$Env:USERPROFILE\.config\alacritty"
-  "$Env:LOCALAPPDATA\nvim" = "$Env:USERPROFILE\.config\nvim"
+  # Alacrity
+  "$APPDATA\alacritty" = "$CONFIG\alacritty"
+  # Neovim
+  "$LOCALAPPDATA\nvim" = "$CONFIG\nvim"
 }
 
 # File symbolic link list: sourceFile => targetFile
 $fileLinks = @{
-  "$Env:USERPROFILE\Documents\PowerShell\Microsoft.PowerShell_profile.ps1" = "$Env:USERPROFILE\.config\powershell\profile.ps1"
+  # Powershell
+  "$DOCUMENTS\PowerShell\Microsoft.PowerShell_profile.ps1" = "$CONFIG\powershell\profile.ps1"
+  # Windows Terminal
+  "$LOCALAPPDATA\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json" = "$CONFIG\windows-terminal\settings.json"
 }
 
-# Create directory symbolic links
-foreach ($dirLink in $dirLinks.GetEnumerator())
+function New-SymbolicLinks
 {
-  $sourceDir = $dirLink.Key
-  $targetDir = $dirLink.Value
+  param (
+    [Parameter(Mandatory=$true)]
+    [Hashtable]$DirectoryLinks,
 
-  # Check if the symbolic link already exists. If it exists, delete it.
-  if (Test-Path $sourceDir)
+    [Parameter(Mandatory=$true)]
+    [Hashtable]$FileLinks
+  )
+
+  # Create directory symbolic links
+  foreach ($dirLink in $DirectoryLinks.GetEnumerator())
   {
-    Remove-Item $sourceDir
+    $sourceDir = $dirLink.Key
+    $targetDir = $dirLink.Value
+
+    # Check if the symbolic link already exists. If it exists, delete it.
+    if (Test-Path $sourceDir)
+    {
+      Remove-Item $sourceDir
+    }
+
+    # Create the symbolic link
+    New-Item -ItemType SymbolicLink -Path $sourceDir -Target $targetDir
   }
 
-  # Create the symbolic link
-  New-Item -ItemType SymbolicLink -Path $sourceDir -Target $targetDir
-}
-
-# Create file symbolic links
-foreach ($fileLink in $fileLinks.GetEnumerator())
-{
-  $sourceFile = $fileLink.Key
-  $targetFile = $fileLink.Value
-
-  # Check if the symbolic link already exists. If it exists, delete it.
-  if (Test-Path $sourceFile)
+  # Create file symbolic links
+  foreach ($fileLink in $FileLinks.GetEnumerator())
   {
-    Remove-Item $sourceFile
-  }
+    $sourceFile = $fileLink.Key
+    $targetFile = $fileLink.Value
 
-  # New-Item -ItemType SymbolicLink -Path $sourceFile -Target $targetFile
-  New-Item -ItemType SymbolicLink -Path $sourceFile -Target $targetFile
+    # Check if the symbolic link already exists. If it exists, delete it.
+    if (Test-Path $sourceFile)
+    {
+      Remove-Item $sourceFile
+    }
+
+    # Create the symbolic link
+    New-Item -ItemType SymbolicLink -Path $sourceFile -Target $targetFile
+  }
 }
+
+# Call the function with the specified symbolic link lists
+New-SymbolicLinks -DirectoryLinks $dirLinks -FileLinks $fileLinks
 
