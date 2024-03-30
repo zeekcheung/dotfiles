4437dad35798fcd2785a6a5ed8d00ab4b864d8a5
zeekcheung <zeekcheung@gmail.com>
2024-03-30 17:49:48 +0800

    chore(nvim): change indent size for vim script

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -146,7 +146,7 @@ autocmd('FileType', {
 -- Change indent size for different filetypes
 autocmd('FileType', {
   group = augroup('change_options', { clear = true }),
-  pattern = { 'c', 'h', 'cpp', 'nu', 'fish' },
+  pattern = { 'c', 'h', 'cpp', 'nu', 'fish', 'vim' },
   callback = function()
     vim.opt_local.tabstop = 4
     vim.opt_local.shiftwidth = 4
