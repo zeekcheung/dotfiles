a480b23553ce0b66c0e844396dcb92db352d086c
zeekcheung <zeekcheung@gmail.com>
2024-04-29 22:03:23 +0800

    feat: add archlinuxcn to /etc/pacmand.conf automatically

--- setup_arch.sh
+++ setup_arch.sh
@@ -11,6 +11,24 @@ sudo bash -c 'sed -i "/# GitHub520 Host Start/Q" /etc/hosts && curl https://raw.
 # github proxy
 ghproxy="https://mirror.ghproxy.com"
 
+pacman_conf="/etc/pacman.conf"
+
+# archlinuxcn repo
+archlinuxcn_name="archlinuxcn"
+archlinuxcn_server="https://mirrors.tuna.tsinghua.edu.cn/archlinuxcn/\$arch"
+
+if ! grep -q "\[${archlinuxcn_name}\]" "$pacman_conf"; then
+	echo "Adding repository ${archlinuxcn_name} to pacman.conf..."
+
+	echo "[${archlinuxcn_name}]" | sudo tee -a "$pacman_conf" >/dev/null
+	echo "Server = ${archlinuxcn_server}" | sudo tee -a "$pacman_conf" >/dev/null
+
+	sudo pacman-key --lsign-key "farseerfc@archlinux.org"
+	sudo pacman -Sy archlinuxcn-keyring --noconfirm
+
+	echo "Repository ${archlinuxcn_name} added to pacman.conf."
+fi
+
 # update and upgrade package sources
 sudo pacman -Syyu --noconfirm
 
