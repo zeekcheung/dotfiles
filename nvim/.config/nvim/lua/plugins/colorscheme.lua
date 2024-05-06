19fd282884f47af194919d55f018120ec3e62ebd
zeekcheung <zeekcheung@gmail.com>
2024-05-06 08:12:15 +0800

    feat(nvim): filename_first path display in telescope

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,6 +1,39 @@
 return {
 
   {
+    'rose-pine/neovim',
+    cond = string.find(vim.g.colorscheme, 'rose-pine', 1, true) ~= nil,
+    lazy = false,
+    priority = 1000,
+    name = 'rose-pine',
+    opts = {
+      styles = {
+        bold = false,
+        transparency = vim.g.transparent_background,
+      },
+      highlight_groups = {
+        -- statusline
+        StatusLine = { fg = 'love', bg = 'love', blend = 10 },
+        StatusLineNC = { fg = 'subtle', bg = 'surface' },
+
+        -- transparent telescope
+        TelescopeBorder = { fg = 'highlight_high', bg = 'none' },
+        TelescopeNormal = { bg = 'none' },
+        TelescopePromptNormal = { bg = 'base' },
+        TelescopeResultsNormal = { fg = 'text' },
+        TelescopeSelection = { fg = 'text', bg = '#393552' },
+        -- TelescopeSelectionCaret = { fg = 'rose', bg = 'rose' },
+
+        -- CmpItemMenu = { bg = "#181825" },
+        CmpBorder = { fg = '#4e4d5d' },
+        CmpDocBorder = { link = 'CmpBorder' },
+        HoverBorder = { link = 'CmpBorder' },
+        SignatureHelpBorder = { link = 'CmpBorder' },
+      },
+    },
+  },
+
+  {
     'catppuccin/nvim',
     cond = string.find(vim.g.colorscheme, 'catppuccin', 1, true) ~= nil,
     lazy = false,
@@ -77,36 +110,4 @@ return {
     },
   },
 
-  {
-    'rose-pine/neovim',
-    cond = string.find(vim.g.colorscheme, 'rose-pine', 1, true) ~= nil,
-    lazy = false,
-    priority = 1000,
-    name = 'rose-pine',
-    opts = {
-      styles = {
-        bold = false,
-        transparency = vim.g.transparent_background,
-      },
-      highlight_groups = {
-        -- statusline
-        StatusLine = { fg = 'love', bg = 'love', blend = 10 },
-        StatusLineNC = { fg = 'subtle', bg = 'surface' },
-
-        -- transparent telescope
-        TelescopeBorder = { fg = 'highlight_high', bg = 'none' },
-        TelescopeNormal = { bg = 'none' },
-        TelescopePromptNormal = { bg = 'base' },
-        TelescopeResultsNormal = { fg = 'subtle', bg = 'none' },
-        TelescopeSelection = { fg = 'text', bg = 'base' },
-        -- TelescopeSelectionCaret = { fg = 'rose', bg = 'rose' },
-
-        -- CmpItemMenu = { bg = "#181825" },
-        CmpBorder = { fg = '#4e4d5d' },
-        CmpDocBorder = { link = 'CmpBorder' },
-        HoverBorder = { link = 'CmpBorder' },
-        SignatureHelpBorder = { link = 'CmpBorder' },
-      },
-    },
-  },
 }
--- nvim/.config/nvim/lua/plugins/util.lua
+++ nvim/.config/nvim/lua/plugins/util.lua
@@ -208,7 +208,11 @@ return {
         defaults = {
           prompt_prefix = '   ',
           selection_caret = '❯ ',
-          path_display = { 'truncate' },
+          path_display = {
+            filename_first = {
+              reverse_directories = false,
+            },
+          },
           sorting_strategy = 'ascending',
           layout_config = {
             horizontal = { prompt_position = 'top', preview_width = 0.55 },
