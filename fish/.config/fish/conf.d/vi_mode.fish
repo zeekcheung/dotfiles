b303b778eac0cd71394b6826e1e89efa48747d35
zeekcheung <zeekcheung@gmail.com>
2024-05-23 11:37:59 +0800

    fix(fish): accept autosuggestions in vi mode

--- fish/.config/fish/conf.d/vi_mode.fish
+++ fish/.config/fish/conf.d/vi_mode.fish
@@ -4,3 +4,10 @@ set -g fish_key_bindings fish_vi_key_bindings
 # Custom bindings
 bind -M insert -m default jj cancel repaint-mode
 set -g fish_sequence_key_delay_ms 200
+
+function fish_user_key_bindings
+    for mode in insert default visual
+        bind -M $mode \cf forward-char
+        bind -M $mode \ef forward-word
+    end
+end
