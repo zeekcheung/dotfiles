881deafdb4f8a2abe1969a9f6a941ab8502389a5
zeekcheung <zeekcheung@gmail.com>
2024-07-02 21:19:48 +0800

    chore: cleanup

--- arch-setup.sh
+++ arch-setup.sh
@@ -4,26 +4,26 @@
 
 DESKTOP_ENVIRONMENT="gnome_minimal" # "gnome" | "gnome_minimal" | "kde"
 INPUT_METHOD="fcitx5"
-DEFAULT_TERMINAL="kitty" # "alacritty" | "kitty" | "wezterm"
+TERMINAL="alacritty" # "alacritty" | "kitty" | "wezterm"
 
 BASE_PACKAGES=(
-	"sof-firmware" "alsa-firmware" "alsa-ucm-conf"         # Audio firmware
-	"bluez" "bluez-utils" "bluez-obex"                     # Bluetooth
-	"ntfs-3g"                                              # ntfs support
-	"adobe-source-han-serif-cn-fonts" "wqy-zenhei"         # Chinese fonts
-	"noto-fonts-cjk" "noto-fonts-emoji" "noto-fonts-extra" # Emoji and extra fonts
-	"firefox" "chromium"                                   # Browsers
-	"git" "wget" "curl"                                    # network tools
+  "sof-firmware" "alsa-firmware" "alsa-ucm-conf"         # Audio firmware
+  "bluez" "bluez-utils" "bluez-obex"                     # Bluetooth
+  "ntfs-3g"                                              # ntfs support
+  "adobe-source-han-serif-cn-fonts" "wqy-zenhei"         # Chinese fonts
+  "noto-fonts-cjk" "noto-fonts-emoji" "noto-fonts-extra" # Emoji and extra fonts
+  "firefox" "chromium"                                   # Browsers
+  "git" "wget" "curl"                                    # network tools
 )
 
 EXTRA_PACKAGES=(
-	"alacritty" "kitty" "wezterm"                                              # Terminal Emulators
-	"zsh" "fish" "man-db"                                                      # Shell
-	"htop" "eza" "fd" "ripgrep" "starship" "yazi" "zoxide" "fastfetch" "stow"  # Terminal tools
-	"neovim" "bob" "tmux" "xsel"                                               # Editor
-	"clang" "cmake" "make" "go" "lua" "luarocks" "nodejs" "yarn" "ruby" "rust" # Languages
-	"rofi" "solaar" "spotify-launcher" "v2raya"                                # Desktop apps
-	"bibata-cursor-theme" "papirus-icon-theme" "pop-icon-theme"                # Desktop themes
+  "$TERMINAL"                                                                # Terminal Emulator
+  "zsh" "fish" "man-db"                                                      # Shell
+  "eza" "fd" "ripgrep" "starship" "yazi" "zoxide" "fastfetch" "stow"         # Terminal tools
+  "neovim" "bob" "tmux" "xsel"                                               # Editor
+  "clang" "cmake" "make" "go" "lua" "luarocks" "nodejs" "yarn" "ruby" "rust" # Languages
+  "rofi" "solaar" "spotify-launcher" "v2raya"                                # Desktop apps
+  "bibata-cursor-theme" "papirus-icon-theme"                                 # Desktop themes
 )
 
 echo "=== Starting Arch Linux setup ==="
@@ -41,13 +41,13 @@ archlinuxcn_name="archlinuxcn"
 archlinuxcn_server="https://mirrors.tuna.tsinghua.edu.cn/archlinuxcn/\$arch"
 
 if ! grep -q "\[${archlinuxcn_name}\]" "$pacman_conf"; then
-	echo "Adding package repository ${archlinuxcn_name} to pacman.conf..."
+  echo "Adding package repository ${archlinuxcn_name} to pacman.conf..."
 
-	echo "[${archlinuxcn_name}]" | sudo tee -a "$pacman_conf" >/dev/null
-	echo "Server = ${archlinuxcn_server}" | sudo tee -a "$pacman_conf" >/dev/null
+  echo "[${archlinuxcn_name}]" | sudo tee -a "$pacman_conf" >/dev/null
+  echo "Server = ${archlinuxcn_server}" | sudo tee -a "$pacman_conf" >/dev/null
 
-	sudo pacman-key --lsign-key "farseerfc@archlinux.org"
-	sudo pacman -Sy archlinuxcn-keyring --noconfirm
+  sudo pacman-key --lsign-key "farseerfc@archlinux.org"
+  sudo pacman -Sy archlinuxcn-keyring --noconfirm
 fi
 
 # Update and upgrade package sources
@@ -81,48 +81,48 @@ paru -S --needed --noconfirm "${EXTRA_PACKAGES[@]}"
 
 # Install desktop environment
 if grep -qi "gnome" "$DESKTOP_ENVIRONMENT"; then
-	if [ "$DESKTOP_ENVIRONMENT" = "gnome_minimal" ]; then
-		# Install minimal gnome
-		paru -S --needed --noconfirm \
-			gnome-shell gnome-control-center gnome-keyring nautilus
-	elif [ "$DESKTOP_ENVIRONMENT" = "gnome" ]; then
-		# Install full gnome
-		paru -S --needed --noconfirm gnome
-	fi
-	# Install other gnome packages
-	paru -S --needed --noconfirm \
-		gdm gnome-tweaks gnome-shell-extensions power-profiles-daemon \
-		gnome-shell-extension-dash-to-dock gnome-shell-extension-forge-git
-	# catppuccin-gtk-theme-macchiato
+  if [ "$DESKTOP_ENVIRONMENT" = "gnome_minimal" ]; then
+    # Install minimal gnome
+    paru -S --needed --noconfirm \
+      gnome-shell gnome-control-center gnome-keyring nautilus
+  elif [ "$DESKTOP_ENVIRONMENT" = "gnome" ]; then
+    # Install full gnome
+    paru -S --needed --noconfirm gnome
+  fi
+  # Install other gnome packages
+  paru -S --needed --noconfirm \
+    gdm gnome-tweaks gnome-shell-extensions power-profiles-daemon \
+    gnome-shell-extension-dash-to-dock gnome-shell-extension-forge-git
+  # catppuccin-gtk-theme-macchiato
 
-	# Enable systemd service
-	sudo systemctl enable gdm
-	sudo systemctl enable power-profiles-daemon
+  # Enable systemd service
+  sudo systemctl enable gdm
+  sudo systemctl enable power-profiles-daemon
 
-	# Replace gnome-console with $DEFAULT_TERMINAL
-	sudo ln -sf /usr/bin/$DEFAULT_TERMINAL /usr/bin/kgx
+  # Replace gnome-console with $DEFAULT_TERMINAL
+  sudo ln -sf /usr/bin/$TERMINAL /usr/bin/kgx
 
-	# Restore dconf settings in gnome
-	bash "$HOME/.dotfiles/bin/.local/bin/dconf-restore"
+  # Restore dconf settings in gnome
+  bash "$HOME/.dotfiles/bin/.local/bin/dconf-restore"
 elif [ "$DESKTOP_ENVIRONMENT" = "kde" ]; then
-	# Install kde
-	paru -S --needed --noconfirm \
-		plasma-meta sddm dolphin ark \
-		p7zip unrar unarchiver lzop lrzip \
-		packagekit-qt5 packagekit appstream-qt appstream \
-		gwenview kate bind
+  # Install kde
+  paru -S --needed --noconfirm \
+    plasma-meta sddm dolphin ark \
+    p7zip unrar unarchiver lzop lrzip \
+    packagekit-qt5 packagekit appstream-qt appstream \
+    gwenview kate bind
 
-	# Enable systemd service
-	sudo systemctl enable sddm
+  # Enable systemd service
+  sudo systemctl enable sddm
 fi
 
 # Install input method
 if [ "$INPUT_METHOD" = "fcitx5" ]; then
-	# Install fcitx5
-	paru -S --needed --noconfirm \
-		fcitx5-im fcitx5-rime fcitx5-chinese-addons fcitx5-material-color fcitx5-pinyin-zhwiki
+  # Install fcitx5
+  paru -S --needed --noconfirm \
+    fcitx5-im fcitx5-rime fcitx5-chinese-addons fcitx5-material-color fcitx5-pinyin-zhwiki
 
-	bash "$HOME/.dotfiles/bin/.local/bin/install-fcitx5"
+  bash "$HOME/.dotfiles/bin/.local/bin/install-fcitx5"
 fi
 
 # Stow packages
